a69afc87c6a480e0b55fed960d75e7a536f86776
drBolsen <al_mamistov@mail.ru>
2014-06-21 10:30:07 +0400

    Basic functionality

---
+++ MyQueue/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyQueue
+{
+    //«Реализовать блокирующую очередь и unit-тесты к ней (на C#).»
+    public class MyQueue<T>// : System.Collections.Generic.ICollection<T>, IEnumerable<T>
+        {
+        private class Node
+            {
+            public T item;
+            public Node next;
+            }
+
+        private Node first;
+        private Node last;
+        private int count=0;
+
+        public int Count { get { return count; } }
+
+        public void Enqueue(T item)
+            {
+            Node oldlast = last;
+            last = new Node();
+            last.item = item;
+            last.next = null;
+            if (first == null) first = last;
+            else        oldlast.next = last;
+            ++count;
+            }
+        public T Dequeue()
+            {
+            try
+            {
+            T item = first.item;
+            first = first.next;
+            if (first == null) last = null;
+            --count;
+            return item;
+              }
+            catch (NullReferenceException exc)
+                {
+                throw new InvalidOperationException("Queue is empty", exc);
+                }
+            }
+     }
+}
---
+++ UnitTestQueue/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestQueue
+    {
+    [TestClass]
+    public class UnitTest1
+        {
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void EmptyTest()
+            {
+            MyQueue.MyQueue<int> queue = new MyQueue.MyQueue<int>();
+            queue.Enqueue(100);
+            Assert.AreEqual(100, queue.Dequeue());
+            queue.Dequeue();
+            }
+        
+        [TestMethod]
+        public void TestMethod1()
+            {
+            MyQueue.MyQueue<int> queue = new MyQueue.MyQueue<int>();
+            queue.Enqueue(100);
+            queue.Enqueue(200);
+            queue.Enqueue(300);
+            Assert.AreEqual(100, queue.Dequeue());
+            Assert.AreEqual(200, queue.Dequeue());
+            Assert.AreEqual(300, queue.Dequeue());
+            }
+        }
+    }
